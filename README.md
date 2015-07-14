4e602444ccacc1ec4ff27484c948ccbc31f4604d
alexshook <alexandra.shook@gmail.com>
2015-07-14 16:24:58 -0400

    initial modifications

--- README.md
+++ README.md
@@ -1,40 +1,23 @@
-# Responsive transactional email designer
+# Listable
 
-Extending the wonderful [Responsive Transactional HTML email templates](https://github.com/mailgun/transactional-email-templates) project. I created a quick jQuery plugin to edit the content of the email in a simple and straight forward manner.
+This is a fork of the [Transactional Email Designer](https://github.com/raulriera/Transactional-Email-Designer) by Raul Riera. It is meant to be used just for lists.
 
-## How to use
+The only dependencies are jQuery and jQuery UI.
 
-* Download the [Responsive Transactional HTML email templates](https://github.com/mailgun/transactional-email-templates) project.
-* Include jQuery and jQuery UI.
-* Include FontAwesome
-* Include the CSS and Javascript provided in this repository
-* Insert the following code into a template
+Add the button:
 
-```html
-<form id="transactional-email-designer-buttons">
-    <button id="title-button" class="fa fa-header"></button>
-    <button id="text-button" class="fa fa-paragraph"></button>
-    <button id="url-button" class="fa fa-link"></button>
-    <button id="clear-button" class="fa fa-trash-o" id="clear-all"></button>
-</form>
-
-<script>
-
-  $( document ).ready(function() {
-    // Init the library
-    $("td.content-wrap > table").transactionalEmailDesigner();
-  });
-
-</script>
+```
+<div class="centered">
+  <button id="add-list-item">+</button>
+</div>
 ```
 
-## Demo
+Initialize the library:
 
-If you want to [see how this works](http://raulriera.github.io/Transactional-Email-Designer).
-
-## Why so many dependencies?
-
-Yeah that sucks, but that is why it's Open Source, let's make it better.
-
-## About:
-Created by Raul Riera, [@raulriera](http://twitter.com/raulriera)
+```
+<script>
+  $().ready(function() {
+    $("td.content-wrap > table").listable();
+  });
+</script>
+```
---
+++ base.js
@@ -0,0 +1,133 @@
+(function( $ ) {
+
+  $.fn.listable = function( options ) {
+    var settings = $.extend({
+      addListItemButton: $('#add-list-item'),
+      listItemPlaceholder: 'New List Item'
+    }, options );
+
+    var currentItem = 1;
+    var list = this;
+    var addListItemButton = settings.addListItemButton;
+
+    // Using the jQuery sortable plugin, let's enable
+    // drag and drop sorting
+    list.sortable({
+      items: "tr",
+      revert: true,
+      cancel: ':input,button,[contenteditable]',
+      helper: function(e, tr)
+      {
+        var $originals = tr.children();
+        var $helper = tr.clone();
+        $helper.children().each(function(index)
+        {
+          // Set helper cell sizes to match the original sizes
+          $(this).width($originals.eq(index).width());
+          $(this).height($originals.eq(index).height());
+        });
+        return $helper;
+      }
+    });
+
+    // Fade In and Fade Out the Remove link on hover
+    list.delegate('tr', 'mouseover mouseout', function(event) {
+      var $this = $(this).find('a.delete');
+
+      if(event.type === 'mouseover') {
+          $this.stop(true, true).fadeIn();
+      } else {
+          $this.stop(true, true).fadeOut();
+      }
+    });
+
+    addListItemButton.click(function(e) {
+      e.preventDefault();
+      addItem('listItem');
+    });
+
+    // Delete item button clicked
+    list.delegate('a.delete', 'click', function(e) {
+      var $this = $(this);
+
+      e.preventDefault();
+      removeItem($this);
+    });
+
+    function removeElement() {
+      return "<a href='#' class='delete'>[x]</a>";
+    }
+
+    function listItemElement() {
+      return "<tr id='item-" + currentItem + "'>"
+      + "<td class='content-block'>"
+      + "<div class='list-item' contenteditable='true'>"
+      + settings.listItemPlaceholder
+      + "</div>" + removeElement()
+      + "</td></tr>";
+    }
+
+    // Add a new item of a given type to the list
+    // this method takes any number of params, depending on the
+    // type of the item to add, the additional params can be used
+    // to send extra information. See 'url' for an example
+    function addItem(type) {
+      switch (type) {
+        case "listItem":
+          list.append( listItemElement() );
+          break;
+        default:
+          throw "Type not implemented";
+          break;
+      }
+
+      // Hide the new item, then fade it in for effects
+      $("#item-" + currentItem)
+        .css('display', 'none')
+        .fadeIn();
+
+      currentItem++;
+    };
+
+    function removeItem($this) {
+      var parentId = $this.parent().attr('id');
+
+      // Fade out the list item then remove from DOM
+      $this.parent().fadeOut(function() {
+          $this.parent().remove();
+      });
+    };
+
+    // function getSelectionText() {
+    // var text = "";
+    // if (window.getSelection) {
+    //     text = window.getSelection().toString();
+    // } else if (document.selection && document.selection.type != "Control") {
+    //     text = document.selection.createRange().text;
+    // }
+    // return text;
+    // }
+
+    // $(".list-item").select(function() {
+    //   alert(this.getSelectionText());
+    // });
+
+    // $(".list-item").select(function() {
+    //   alert('selected');
+    // });
+
+    $('.list-item').on('click', function(){
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+
+    alert(text);
+});
+
+    return this;
+
+  };
+}( jQuery ));
